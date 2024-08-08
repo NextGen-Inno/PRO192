8221333690e329b68375547dfdcd71ce1dfa66b2
Nguyen Minh Duc <nminhducit@gmail.com>
2024-08-08 14:54:46 +0700

    Create Day4

---
+++ Day4/src/Student.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DUKE
+ */
+public class Student {
+    private String stdID;
+    private String name; 
+    private int age;
+    private float GPA;
+
+    public Student() {
+    
+    }
+        
+    public Student(String stdID, String name, int age, float GPA) {
+        this.stdID = stdID;
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+   
+    
+}
