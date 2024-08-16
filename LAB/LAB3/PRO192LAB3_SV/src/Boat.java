e8f5b05674c82477297b6d0d78b0b3222605086c
Nguyen Minh Duc <nminhducit@gmail.com>
2024-08-16 23:03:31 +0700

    Update

--- LAB/LAB3/PRO192LAB3_SV/src/Boat.java
+++ LAB/LAB3/PRO192LAB3_SV/src/Boat.java
@@ -8,5 +8,44 @@
  * @author DUKE
  */
 public class Boat {
-    
+
+    private String name;
+    private String color;
+    private double price;
+
+    public Boat(String name, String color, double price) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "MyBoat name= " + getName() + ", color= " + getColor() + ", price= " + getPrice() + ", model= " + model;
+    }
+  
 }
--- LAB/LAB3/PRO192LAB3_SV/src/MyBoat.java
+++ LAB/LAB3/PRO192LAB3_SV/src/MyBoat.java
@@ -7,6 +7,77 @@
  *
  * @author DUKE
  */
-public class MyBoat {
+
+import java.util.stream.Collectors;
+
+public class MyBoat extends Boat {
+    private String model;
+
+    public MyBoat(String name, String color, double price, String model) {
+        super(name, color, price);
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void MyInfor() {
+        System.out.println("He180274 - NGUYEN MINH DUC");
+    }
+    
+    public double getSalePrice() {
+        if (model.length() > 0 && Character.toUpperCase(model.charAt(model.length() - 1)) == 'A') {
+            return getPrice() * 0.95;
+        }
+        return getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return "MyBoat name= " + getName() + ", color= " + getColor() + ", price= " + getPrice() + ", model= " + getModel();
+    } 
+
+    public int getGeneration() {
+        if (model.length() >= 8) {
+            String year = model.substring(1, 3);
+            return 2000 + Integer.parseInt(year);
+        }
+        return 0;
+    }
+
+    public String getVersion() {
+        if (model.length() >= 6) {
+            String versionP = model.substring(5, 6);
+            return "V" + versionP;
+        }
+        return " "; 
+    }
+
+    public void getCharModel() {
+        String letters = model.replaceAll("[^A-Z]", "");
+        String sortedLetters = letters.chars()
+                .mapToObj(c -> (char) c)
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+        System.out.println(sortedLetters);
+    }
+
+    public void getDigitsModel() {
+        String digits = model.replaceAll("[^0-9]", "");
+        String sortedDigits = digits.chars()
+                .mapToObj(c -> (char) c)
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+        System.out.println(sortedDigits);
+    }
     
 }
+
+
